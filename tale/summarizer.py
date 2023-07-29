8b222e17814b1df1a614accfaef2ec0b663ec7bf
Simon Gimmini <Gimmini@stud.uni-heidelberg.de>
2023-07-29 23:35:20 +0200

    poor attempt to summarize with gpt

--- tale/summarizer.py
+++ tale/summarizer.py
@@ -10,26 +10,27 @@ def main():
     secret_location = os.path.join(working_dir, "secret.json")
 
     with open(secret_location, encoding="UTF-8") as json_file:
-        api_key = json.load(json_file)["api_key"]
-
-    openai.api_key = os.getenv(api_key)
+        openai.api_key = json.load(json_file)["api_key"]
 
     processor = pre_processor.PreProcessor() 
-    notes = processor.parse_textfolder("/Users/simongimmini/Documents/projects/T.A.L.E./consumer/the_sprawl")
-
-    print(notes)
+    notes = processor.parse_textfolder(os.path.join(working_dir, "consumer/the_sprawl"))
 
     messages = [
-        {"role": "system", "content": "Du bist ein leistungsfähiges Modell, das Notizen von verschiedenen Personen zusammenfassen und verschmelzen möchte. Du wirst Überschneidungen finden und herausfinden, wo fehlende Notizen sich einander ergänzen."},
+        {"role": "system", "content": "Du bist ein leistungsfähiges Modell, das Notizen von verschiedenen Personen zusammenfassen und verschmelzen möchte. Du wirst Überschneidungen finden und herausfinden, wo fehlende Notizen sich einander ergänzen. Die Zusammenfassung soll mindestens 2000 Worte umfassen. Die Zusammenfassung ist in Stichpunkten."},
         {"role": "user", "content": f"{notes}"}
     ]
 
-    # response = openai.ChatCompletion.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[],
-    #     temperature=0,
-    #     max_tokens=1024
-    # )
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0,
+        max_tokens=4096
+    )
+
+    # write response to file "response.json"
+    with open("response.json", "w", encoding="UTF-8") as json_file:
+        json.dump(response, json_file, indent=4)
+
 
 
 if __name__ == "__main__":
