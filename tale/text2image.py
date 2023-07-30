51ceb5a047535344bb7e538475a8192e4d8d46ae
Florian Rauls <34367590+FlorianRauls@users.noreply.github.com>
2023-07-30 12:48:52 +0200

    Reworked text2image into a workable class Co-authored-by: Simon Gimmini <smn57@users.noreply.github.com> Co-authored-by: mlunihd <mlunihd@users.noreply.github.com>

--- tale/text2image.py
+++ tale/text2image.py
@@ -1,48 +1,47 @@
 from diffusers import DiffusionPipeline
 import torch
-from PIL import Image
-
-# set cuda DEVICE
-DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
-NUM_STEPS = 3
-RESOLUTION = 80
 
 
-def text2image(prompt):
+class Text2Image:
     """
-    This function takes a prompt as input and returns an image as output.
-    :param prompt: a string of text
-    :return: an image
+    This class takes a prompt as input and returns an image as output.
     """
-    pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float32).to(DEVICE)
-    generator = torch.Generator().manual_seed(42)
-    height = 3 * RESOLUTION
-    width = 4 * RESOLUTION
-    image = pipe(prompt, height=height, width=width, num_inference_steps= NUM_STEPS, generator=generator).images[0]
-    return image
+
+    def __init__(self) -> None:
+        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.model = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float32).to(device)
+
+    def text2image(self, prompt, num_steps=3, resolution=80):
+        """
+        This function takes a prompt as input and returns an image as output.
+        :param prompt: a string of text
+        :return: an image
+        """
+        generator = torch.Generator().manual_seed(42)
+        height = 3 * resolution
+        width = 4 * resolution
+        generated_image = self.model(prompt, height=height, width=width, num_inference_steps=num_steps, generator=generator).images[0]
+        return generated_image
 
 
-# a function to save the image
-def save_image(image):
-    """
-    This function takes an image as input and saves it to a file.
-    :param image: an image
-    :return: None
-    """
-    # save the image
-    image.save("cat.png")
-    return
+    def save_image(self, image, path):
+        """
+        This function takes an image as input and saves it to a file.
+        :param image: an image
+        :return: None
+        """
+        # save the image
+        image.save(path)
+        return
 
-def main():
-    """
-    This function runs the main program.
-    :return: None
-    """
-    prompt = "A painting of a cat"
-    image = text2image(prompt)
-    save_image(image)
-    return
-
-# run main function
 if __name__ == "__main__":
-    main()
+    # create an instance of the class
+    text2image = Text2Image()
+    # get the prompt
+    TEST_PROMPT = "A dog on a pillow"
+    # generate the image
+    test_image = text2image.text2image(TEST_PROMPT)
+    # save the image
+    text2image.save_image(test_image, "test_image.png")
+    # show the test_image
+    test_image.show()
