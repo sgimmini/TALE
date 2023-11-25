560276b464eea42b58e0a54576793ad835519c29
Florian Rauls <frauls@web.de>
2023-11-25 16:01:24 +0100

    Differentiation between diffusion and api
    Made sure that api / diffuer only initializes if needed

--- tale/text2image.py
+++ tale/text2image.py
@@ -6,30 +6,52 @@ import base64
 import requests
 from PIL import Image
 
-# read api key from file secret.txt
-with open("secret.txt", "r") as file:
-    api_key = file.read()
-client = OpenAI(api_key=api_key)
-
-
 class Text2Image:
     """
     This class takes a prompt as input and returns an image as output.
     """
 
     def __init__(self) -> None:
+        device = None
+        self.pipe = None
+        self.client = None
+        client = None
+        
+        is_openai = False
+        is_diffusion = False
+
+
+    def init_diffusion(self):
+        """
+        This function initializes the diffusion pipeline.
+        :return: None
+        """
         device = "cuda"
         self.pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16).to(device)
         self.pipe.safety_checker =  None
         self.pipe.scheduler = KDPM2AncestralDiscreteScheduler.from_config(self.pipe.scheduler.config, rescale_betas_zero_snr=True, timestep_spacing="trailing")
+        return
+    
+    def init_openai(self):
+        """
+        This function initializes the openai api.
+        :return: None
+        """
+        # read api key from file secret.txt
+        with open("secret.txt", "r") as file:
+            api_key = file.read()
+        self.client = OpenAI(api_key=api_key)
+        return
 
-
-    def text2image(self, prompt, num_steps=5, resolution=256):
+    def text2image_diffusion(self, prompt, num_steps=5, resolution=256):
         """
         This function takes a prompt as input and returns an image as output.
         :param prompt: a string of text
         :return: an image
         """
+        
+        if self.pipe is None:
+            self.init_diffusion()
         
         generator = torch.Generator().manual_seed(42)
         generated_image = self.pipe(prompt, height=resolution, width=resolution, num_inference_steps=num_steps, generator=generator).images[0]
@@ -52,7 +74,11 @@ class Text2Image:
         :param prompt: a string of text
         :return: an image
         """
-        response = client.images.generate(
+        
+        if self.client is None:
+            self.init_openai()
+        
+        response = self.client.images.generate(
             model="dall-e-3",
             prompt=prompt,
             size=resolution,
