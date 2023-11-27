589ca982369e4b4fc865c4abe185fba42b30b723
Florian Rauls <frauls@web.de>
2023-11-27 11:04:42 +0100

    Created main pipeline file

---
+++ tale/tale.py
@@ -0,0 +1,23 @@
+from text2image import Text2Image
+from summarizer import Summarizer
+from post_processor import PostProcessor
+
+
+
+# main function
+if __name__ == "__main__":
+    # create an instance of the class
+    text2image = Text2Image()
+    summarizer = Summarizer()
+    postProc = PostProcessor()
+    
+    # get prompts from the notes
+    prompts = summarizer.getPropmtsFromNotes("data//dummy//input//input.txt")
+    
+    
+    # generate images from the prompts
+    text2image.generateImages(prompts)
+    
+    # generate html file from the images
+    postProc.generateHtmlFile()
+    
