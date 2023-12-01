f42a3d7f46c22f2add6b31bd6b0c346ce98b51db
Florian Rauls <frauls@web.de>
2023-12-01 23:52:03 +0100

    Forgot to comment out :)

--- tale/tale.py
+++ tale/tale.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     
     # get prompts from the notes
     
-    #prompts, indexedStory = summarizer.getPropmtsFromNotes("data//input//notes//")
+    prompts, indexedStory = summarizer.getPropmtsFromNotes("data//input//notes//")
     # write the prompts to a json file
-    #f = open("data//input//gpt_created_prompts.json", "w")
-    #f.write(prompts)
-    #f.close()
+    f = open("data//input//gpt_created_prompts.json", "w")
+    f.write(prompts)
+    f.close()
     
     # read the prompts from a json file
     f = open("data//input//gpt_created_prompts.json", "r")
@@ -25,9 +25,9 @@ if __name__ == "__main__":
     
     
     # write the indexed story to a file
-    #f = open("data//input//indexed_story.txt", "w")
-    #f.write(indexedStory)
-    #f.close()
+    f = open("data//input//indexed_story.txt", "w")
+    f.write(indexedStory)
+    f.close()
     
     
     # read the indexed story from a file
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     
     
     # generate images from the prompts
-    #text2image.generateImages("data//input//gpt_created_prompts.json")
+    text2image.generateImages("data//input//gpt_created_prompts.json")
     
     # generate html file from the images
     postProc.process(indexedStory)
