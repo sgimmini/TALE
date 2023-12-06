6df7371c63b036e87b22fa0f0d01cd03be791ed1
Florian Rauls <frauls@web.de>
2023-12-06 08:53:57 +0100

    Followed wise advise about files

--- tale/tale/tale.py
+++ tale/tale/tale.py
@@ -11,29 +11,29 @@ def runPipeline():
     postProc = PostProcessor()
 
     # get prompts from the notes
-    prompts, indexedStory = summarizer.getPropmtsFromNotes(os.path.join(os.getcwd(), "data", "input", "notes"))
+    prompts, indexedStory = summarizer.getPropmtsFromNotes(__file__, "data", "input", "notes"))
     # write the prompts to a json file
-    f = open(os.path.join(os.getcwd(), "data", "input", "gpt_created_prompts.json"), "w")
+    f = open(__file__, "data", "input", "gpt_created_prompts.json"), "w")
     f.write(prompts)
     f.close()
 
     # read the prompts from a json file
-    f = open(os.path.join(os.getcwd(), "data", "input", "gpt_created_prompts.json"), "r")
+    f = open(__file__, "data", "input", "gpt_created_prompts.json"), "r")
     prompts = f.read()
     f.close()
 
     # write the indexed story to a file
-    f = open(os.path.join(os.getcwd(), "data", "input", "indexed_story.txt"), "w")
+    f = open(__file__, "data", "input", "indexed_story.txt"), "w")
     f.write(indexedStory)
     f.close()
 
     # read the indexed story from a file
-    f = open(os.path.join(os.getcwd(), "data", "input", "indexed_story.txt"), "r")
+    f = open(__file__, "data", "input", "indexed_story.txt"), "r")
     indexedStory = f.read()
     f.close()
 
     # generate images from the prompts
-    text2image.generateImages(os.path.join(os.getcwd(), "data", "input", "gpt_created_prompts.json"))
+    text2image.generateImages(__file__, "data", "input", "gpt_created_prompts.json"))
 
     # generate html file from the images
     postProc.process(indexedStory)
